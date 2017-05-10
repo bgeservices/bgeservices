37034326283ad4baa3014be5dd17b44450c907e1
Geno Roupsky <geno@roupsky.name>
2017-05-10 21:44:41 +0300

    integrate raven

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// if (process.env.NODE_ENV === 'production') {
-//   var Vue = require('vue')
-//   var Raven = require('raven-js')
-//   var RavenVue = require('raven-js/plugins/vue')
-//   Raven
-//     .config('https://93be86fbce7f4ebab4d9e0abf3dcee17@sentry.io/145052')
-//     .addPlugin(RavenVue, Vue)
-//     .install()
-// }
+if (process.env.NODE_ENV === 'production') {
+  var Vue = require('vue')
+  var Raven = require('raven-js')
+  var RavenVue = require('raven-js/plugins/vue')
+  Raven
+    .config('https://77515e99a036430f834eb0263e0e7215@sentry.io/166996')
+    .addPlugin(RavenVue, Vue)
+    .install()
+}
 
 var app = module.exports = {
   services: require('./src/services'),
